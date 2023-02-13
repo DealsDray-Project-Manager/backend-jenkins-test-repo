2b38cda8c91623de20f27a4bf4f4f85a11de5706
Rafnas <mohd.rafnas@dealsdray.com>
2023-02-13 19:26:54 +0530

    Done

--- Router/warehouseInRouter/warehouseIn.js
+++ Router/warehouseInRouter/warehouseIn.js
@@ -1761,4 +1761,200 @@ router.post("/salesBinItem/search/:uic", async (req, res, next) => {
     next(error);
   }
 });
+
+
+/*-------------------------------------AUDIT USER STATUS CHECKING------------------------------------------------------------*/
+router.post("/auditUserStatusChecking/:username/:brand/:model", async (req, res, next) => {
+  try {
+    const { username,brand,model } = req.params;
+    let data = await warehouseInController.checkAuditUserFreeOrNot(username,brand,model);
+    console.log(data);
+    if (data.status === 1) {
+      res.status(200).json({
+        data: "User is free",
+      });
+    } else if (data.status === 2) {
+      res.status(200).json({
+        data: "Agent already have a lot",
+      });
+    } else if (data.status === 3) {
+      res.status(200).json({
+        data: "User not active",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+/*----------------------------AUDIT TRAY ASSIGN WITH OTHER TRAY CHECKING------------------------------------------------------*/
+
+router.post(
+  "/trayIdCheckAuditApprovePage/:trayId/:trayType/:location/:brand/:model",
+  async (req, res, next) => {
+    try {
+      const { trayId, trayType, location, brand, model } = req.params;
+
+      let data = await warehouseInController.checkTrayStatusAuditApprovePage(
+        trayId,
+        trayType,
+        location,
+        brand,
+        model
+      );
+      console.log(data);
+      if (data.status == 1) {
+        res.status(200).json({
+          message: "Valid Tray",
+          trayId: trayId,
+        });
+      } else if (data.status == 2) {
+        res.status(202).json({
+          message: `Not a ${trayType} tray`,
+        });
+      } else if (data.status == 4) {
+        res.status(202).json({
+          message: "Tray id does not exists",
+        });
+      } else if (data.status == 5) {
+        res.status(202).json({
+          message: "Mismatch Brand and Model",
+        });
+      } else {
+        res.status(202).json({
+          message: "Tray already in process",
+        });
+      }
+    } catch (error) {}
+  }
+);
+
+/*----------------------------------------AUDIT TRAY ISSUE TO AGENT----------------------------------------------- */
+
+router.post("/auditTrayIssueToAgent", async (req, res, next) => {
+  console.log(req.body);
+  try {
+    const data = await warehouseInController.auditTrayAssign(req.body);
+    if (data.status == 1) {
+      res.status(200).json({
+        message: "Successfully Assigned to Agent",
+      });
+    } else {
+      res.status(202).json({
+        message: "Request Failed please tray again",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+/*-------------------------------------FETCH ASSIGNED OTHER TRAY--------------------------------------------*/
+
+router.post("/fetchAssignedTrayForAudit/:username/:brand/:model", async (req, res, next) => {
+  try {
+    const { username,brand,model } = req.params;
+    let data = await warehouseInController.getAssignedTrayForAudit(username,brand,model);
+    console.log(data);
+    if (data) {
+      res.status(200).json({
+        data: data,
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+/*-----------------------------------------RETURN FROM AUDIT WHT RELEASE----------------------------------------------------*/
+router.post("/wht-relase/:trayId", async (req, res, next) => {
+  try {
+    const { trayId } = req.params;
+    let data = await warehouseInController.whtTrayRelease(trayId);
+    if (data) {
+      res.status(200).json({
+        message: "Successfully Released",
+      });
+    } else {
+      res.status(202).json({
+        message: "Failed",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+/*----------------------------------------------------AUDIT USER TRAY FOR ASSIGN------------------------------------------------*/
+
+router.post("/auditUserTrayForAssign", async (req, res, next) => {
+  try {
+    const { username, tray_type, tray_id, location } = req.body;
+    console.log(req.body);
+    let data = await warehouseInController.auditUserTray(
+      username,
+      tray_type,
+      tray_id,
+      location
+    );
+    console.log(data);
+    if (data.status === 1) {
+      res.status(200).json({
+        message: "Valid",
+        trayStatus: data.tray_status,
+      });
+    } else if (data.status === 2) {
+      res.status(202).json({
+        message: `User have already ${tray_type} - tray`,
+        trayStatus: data.tray_status,
+      });
+    } else if (data.status == 3) {
+      res.status(202).json({
+        message: `Not a  ${tray_type} tray`,
+        trayStatus: data.tray_status,
+      });
+    } else if (data.status == 4) {
+      res.status(202).json({
+        message: `Tray id does not exists`,
+        trayStatus: "",
+      });
+    } else if (data.status == 5) {
+      res.status(202).json({
+        message: `Tray is in process`,
+        trayStatus: data.tray_status,
+      });
+    } else if (data.status == 6) {
+      res.status(202).json({
+        message: `User have no issued WHT`,
+        trayStatus: "",
+      });
+    } else if (data.status == 7) {
+      res.status(202).json({
+        message: `Mismatch Model or Brand`,
+        trayStatus: "",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+/* -------------------------TRAY ASSIGN TO AUDIT------------------------------*/
+router.post("/oneTrayAssigToAudit", async (req, res, next) => {
+  try {
+    let data = await warehouseInController.oneTrayAssignToAudit(req.body);
+    if (data) {
+      res.status(200).json({
+        message: "Successfully Assigned",
+      });
+    } else {
+      res.status(200).json({
+        message: "Failed tray again",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
