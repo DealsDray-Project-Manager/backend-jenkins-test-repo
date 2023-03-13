25d6e7e46adb0fc094b180e656eefe6612726a1e
Rafnas <mohd.rafnas@dealsdray.com>
2023-03-13 16:15:12 +0530

    Done

--- Controller/superAdmin/superAdminController.js
+++ Controller/superAdmin/superAdminController.js
@@ -13,8 +13,8 @@ const {
 } = require("../../Model/masterHistoryModel/mastersHistory");
 const moment = require("moment");
 
-const IISDOMAIN = "http://prexo-v7-2-uat-api.dealsdray.com/user/profile/";
-const IISDOMAINPRDT = "http://prexo-v7-2-uat-api.dealsdray.com/product/image/";
+const IISDOMAIN = "http://prexo-testing-api.dealsdray.com/user/profile/";
+const IISDOMAINPRDT = "http://prexo-testing-api.dealsdray.com/product/image/";
 
 /************************************************************************************************** */
 
