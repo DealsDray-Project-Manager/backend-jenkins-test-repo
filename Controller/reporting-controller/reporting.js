b2214e881bfc7657c4a618d280dfb1af982dc445
Rafnas <mohd.rafnas@dealsdray.com>
2023-03-29 11:56:46 +0530

    Reporting panel in processing

--- Controller/reporting-controller/reporting.js
+++ Controller/reporting-controller/reporting.js
@@ -1,4 +1,5 @@
 const { delivery } = require("../../Model/deliveryModel/delivery");
+const {orders}=require("../../Model/ordersModel/ordersModel")
 /*--------------------------------------------------------------*/
 
 /************************************************** */
@@ -6,14 +7,58 @@ const { delivery } = require("../../Model/deliveryModel/delivery");
 /************************************************** */
 
 module.exports = {
-  getUnitsCond: (location, limit, skip) => {
+  dashboardData: (location) => {
     return new Promise(async (resolve, reject) => {
-      const units = await delivery
-        .find({ partner_shop: location })
-        .limit( limit )
-        .skip(skip);
-      const forCount = await delivery.count({ partner_shop: location });
-      resolve({ units: units, forCount: forCount });
+      let count = {
+        allOrders: 0,
+        deliveredOrder: 0,
+        notDeliveredOrders: 0,
+        processingUnits: 0,
+        readyForSale: 0,
+      };
+      count.allOrders = await orders.count({ partner_shop: location });
+      count.notDeliveredOrders = await orders.count({
+        partner_shop: location,
+        delivery_status: "Pending",
+      });
+      count.deliveredOrder = await orders.count({
+        partner_shop: location,
+        delivery_status: "Delivered",
+      });
+      count.processingUnits = await delivery.count({
+        partner_shop: location,
+        ctx_tray_id: { $exists: false },
+      });
+      count.readyForSale = await delivery.count({
+        partner_shop: location,
+        ctx_tray_id: { $exists: true },
+      });
+      if (count) {
+        resolve(count);
+      }
+    });
+  },
+  getUnitsCond: (location, limit, skip, screen) => {
+    console.log(screen);
+    return new Promise(async (resolve, reject) => {
+      if (screen == "Processing-units") {
+        const units = await delivery
+          .find({ partner_shop: location, ctx_tray_id: { $exists: false } })
+          .limit(limit)
+          .skip(skip);
+        const forCount = await delivery.count({ partner_shop: location });
+        resolve({ units: units, forCount: forCount });
+      } else {
+        const units = await delivery
+          .find({ partner_shop: location })
+          .limit(limit)
+          .skip(skip);
+        const forCount = await delivery.count({
+          partner_shop: location,
+          ctx_tray_id: { $exists: true },
+        });
+        resolve({ units: units, forCount: forCount });
+      }
     });
   },
 };
--- Router/reporting-router/reporting.js
+++ Router/reporting-router/reporting.js
@@ -3,17 +3,32 @@ const express = require("express");
 const router = express.Router();
 const reportingAgentRouter = require("../../Controller/reporting-controller/reporting");
 /*-----------------------------ROUTERS------------------------------------*/
+//DASHBOARD 
+router.post("/dashboard/:location", async (req, res, next) => {
+    try {
+      const { location } = req.params;
+      let data = await reportingAgentRouter.dashboardData(location);
+      if (data) {
+        res.status(200).json({
+          data: data,
+        });
+      }
+    } catch (error) {
+      next(error);
+    }
+  });
 // UNITS BASED FILTER
-router.post("/units/:location/:page/:size", async (req, res, next) => {
+router.post("/units/:location/:page/:size/:screen", async (req, res, next) => {
   try {
-    let { location, page, size } = req.params;
+    let { location, page, size,screen } = req.params;
     page++;
     const limit = parseInt(size);
     const skip = (page - 1) * size;
     const unitsData = await reportingAgentRouter.getUnitsCond(
       location,
       limit,
-      skip
+      skip,
+      screen
     );
     if (unitsData) {
       res.status(200).json({
